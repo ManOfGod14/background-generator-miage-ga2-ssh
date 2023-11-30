ddbcae1ad09f438384b138f63ad5e07456c11164
ManOfGod14 <maliki.tcherou1@gmail.com>
2023-11-30 15:35:17 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// console.log(window.document);
+
+// récupération de l'élément input par son id firstColor
+const firstColorElt = document.getElementById('firstColor');
+// console.log(firstColorElt);
+
+// récupération de l'élément input par son id secondColor
+const secondColorElt = document.getElementById('secondColor');
+
+// récupération de l'élément body
+const bodyElt = document.querySelector('body');
+console.log(bodyElt);
+
+//
+firstColorElt.addEventListener('input', function() {
+    // console.log(firstColorElt.value);
+    bodyElt.style = "background: linear-gradient(to right,"+ firstColorElt.value +", "+ secondColorElt.value +")";
+});
+
+secondColorElt.addEventListener('input', function() {
+    // console.log(secondColorElt.value);
+    bodyElt.style = `background: linear-gradient(to right, ${firstColorElt.value}, ${secondColorElt.value});`
+});
