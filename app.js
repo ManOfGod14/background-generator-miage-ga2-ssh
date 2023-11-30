7a51a252dd9d4bce0ea83b3e5cecf033ddfa597b
ManOfGod14 <maliki.tcherou1@gmail.com>
2023-11-30 15:57:12 +0100

    Index modifié

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // console.log(window.document);
 
+
 // récupération de l'élément input par son id firstColor
 const firstColorElt = document.getElementById('firstColor');
 // console.log(firstColorElt);
